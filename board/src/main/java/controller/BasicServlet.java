bbd9a291b91ec481774218f0310b65aad076e71e
이교웅 <landjjang1212@gmail.com>
2024-10-21 18:07:54 +0900

    board webapp

--- board/src/main/java/controller/BasicServlet.java
+++ board/src/main/java/controller/BasicServlet.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import action.Action;
 import action.ActionForward;
+import action.BoardListAction;
+import action.BoardReadAction;
+import action.BoardUpdateAction;
 
 /**
  * Servlet implementation class BasicServlet
@@ -36,6 +39,17 @@ public class BasicServlet extends HttpServlet {
 		
 		Action action = null;
 		
+		if (cmd.equals("/list.do")) { // 목록
+			action = new BoardListAction("/board/list.jsp");
+		} else if(cmd.equals("/read.do")) { // 상세조회
+			action = new BoardReadAction("/board/read.jsp");
+		} else if(cmd.equals("/modify.do")) { // 수정화면
+			action = new BoardReadAction("/board/modify.jsp");
+		}  else if(cmd.equals("/update.do")) { // 업데이트 실제 수행
+			// 수정 성공 후 상세조회로 이동
+			action = new BoardUpdateAction("/read.do");
+		}
+
 
 		ActionForward af = null;
 
